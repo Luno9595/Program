8678fdf79eca7aaf36a335a88cf1ec99470db8bf
Luno <dnikonenok@gmail.com>
2024-08-02 11:06:57 +0000

    Добавлена строка которая вызывает библиотеку а также функция которая выводит размер свап-файла

--- program.py
+++ program.py
@@ -1,8 +1,15 @@
 import psutil
+import subprocess
 
 def get_disk():
     partitions = pautil.disk_partitions()
     for partition in partitions:
         usage = psutil.disk_usage(partition.mountpoint)
         print(f"Free: {usage.free / (1024 ** 3):.2f} GB")
+        
+def get_swap():
+    result = subprocess.run(["swapon", "-s"], capture_output=True, text=True)
+    print(result.stdout)
+        
+get_swap_info()
 get_disk()
